76d441959c3b3c81ddf996495ee91034eebaa34f
krisha <krishachaklasiya208@gmail.com>
2026-01-03 13:38:53 +0530

    add implement

--- sum.java
+++ sum.java
@@ -3,7 +3,7 @@ class addition {
         int a= 10;
         int b= 20;
         int sum = a + b;
-
+        int sub = a - b;
         System.out.println("Sum = " + sum);
         System.out.println("Sum = " + sum);
     }
